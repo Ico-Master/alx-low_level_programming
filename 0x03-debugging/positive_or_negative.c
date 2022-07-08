d811a26c88a886024bb6e8e1d77e7107068188a9
Ico-Master <61706897+Ico-Master@users.noreply.github.com>
2022-07-08 01:27:42 +0100

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 
-void positive_or_negative(int i){
-	if(i < 0){
-	     printf( "%d is negative",i);
-	}else if( i > 0 )
-		printf("%d is positive",i);
-	else{
-	     printf( "0 is zero");
-	}
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf( "%d is negative", i);
+	else if ( i > 0)
+		printf("%d is positive", i);
+	else 
+		printf("0 is zero");
+	
 }
 
 
